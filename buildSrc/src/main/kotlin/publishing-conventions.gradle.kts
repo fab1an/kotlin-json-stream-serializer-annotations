58c254a4b354759f71d2c447bf1137e06ccc27a1
Fabian Zeindl <fabian.zeindl@gmail.com>
2025-03-24 06:53:33 +0100

    [build] add LICENSE and publication

---
+++ buildSrc/src/main/kotlin/publishing-conventions.gradle.kts
@@ -0,0 +1,85 @@
+import java.util.Properties
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.secretKeyRingFile"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply {
+            load(it)
+        }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(javadocJar.get())
+
+        pom {
+            name.set("kotlin-json-stream-serializer annotations")
+            description.set("Annotations for kotlin-json-stream-serializer")
+            url.set("https://github.com/fab1an/kotlin-json-stream-serializer-annotations")
+
+            licenses {
+                license {
+                    name.set("Apache-2.0")
+                    url.set("https://opensource.org/license/apache-2-0/")
+                }
+            }
+            developers {
+                developer {
+                    id.set("https://github.com/fab1an/")
+                    name.set("Fabian Zeindl")
+                    email.set("fabian@fabianzeindl.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/fab1an/kotlin-json-stream-serializer-annotations")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
+
+val signingTasks = tasks.withType<Sign>()
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    dependsOn(signingTasks)
+}
